2727248f05c4388575457faf68475745a266ef7a
AlexMules <alexandru.mules@gmail.com>
2026-01-09 12:45:26 +0200

    Added app logo

--- src/main/java/org/alex/donor/JavaFxApplication.java
+++ src/main/java/org/alex/donor/JavaFxApplication.java
@@ -2,9 +2,12 @@ package org.alex.donor;
 
 import javafx.application.Application;
 import javafx.application.Platform;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.ConfigurableApplicationContext;
+
+import java.io.InputStream;
 
 public class JavaFxApplication extends Application {
 
@@ -20,7 +23,17 @@ public class JavaFxApplication extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-        // Trimite un semnal către Spring că interfața este gata să apară
+        // 1. Încărcăm fluxul de date al imaginii
+        InputStream iconStream = getClass().getResourceAsStream("/images/logo_app.png");
+
+        // 2. Verificăm dacă imaginea a fost găsită și o adăugăm la iconițele ferestrei
+        if (iconStream != null) {
+            primaryStage.getIcons().add(new Image(iconStream));
+        } else {
+            System.err.println("Atenție: Imaginea /images/logo_app.png nu a fost găsită!");
+        }
+
+        // 3. Trimitem semnalul către Spring
         context.publishEvent(new StageReadyEvent(primaryStage));
     }
 
